9ecfff683d4cb3fb2cb4c667db3a38375072ca7f
Hanzala Aqeel <connect.hanzala@gmail.com>
2022-07-05 20:15:38 +0500

    AVERAGE CALCULATOR

--- average.js
+++ average.js
@@ -1,15 +1,15 @@
 function calculate() {
 
-    let value1 = document.querySelector("#value 1").value;
-    let value2 = document.querySelector("#value 2").value;
-    let value3 = document.querySelector("#value 3").value;
-    let value4 = document.querySelector("#value 4").value;
-    let value5 = document.querySelector("#value 5").value;
-    let value6 = document.querySelector("#value 6").value;
-    let value7 = document.querySelector("#value 7").value;
-    let value8 = document.querySelector("#value 8").value;
-    let value9 = document.querySelector("#value 9").value;
-    let value10 = document.querySelector("#value 10").value;
+    let value1 = document.querySelector("#value1").value;
+    let value2 = document.querySelector("#value2").value;
+    let value3 = document.querySelector("#value3").value;
+    let value4 = document.querySelector("#value4").value;
+    let value5 = document.querySelector("#value5").value;
+    let value6 = document.querySelector("#value6").value;
+    let value7 = document.querySelector("#value7").value;
+    let value8 = document.querySelector("#value8").value;
+    let value9 = document.querySelector("#value9").value;
+    let value10 = document.querySelector("#value10").value;
 
     let sum = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10 ;
     let average = sum / 10 ;
